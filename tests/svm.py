07de4f8067acb619a5d3610f6c481ba4a340d79b
said <eger3612@gmail.com>
2021-07-19 22:04:17 +0200

    fix : fix test

--- tests/svm.py
+++ tests/svm.py
@@ -22,13 +22,14 @@ class MyTestCase(unittest.TestCase):
         data = pd.DataFrame(result)
         self.assertIsInstance(data, pd.DataFrame)
 
+    # def test_run_grid_search_output(self):
+    # result = self.data.run_grid_search()
+    # expected = self.data.grid.cv_results_
+    # np.testing.assert_allclose(result, expected, rtol=0.2, atol=0)
 
-    #def test_run_grid_search_output(self):
-        #result = self.data.run_grid_search()
-        #expected = self.data.grid.cv_results_
-        #np.testing.assert_allclose(result, expected, rtol=0.2, atol=0)
-
-    #def test_save(self):
-        #actual_get_score = self.data.save()
-        #expected_get_score = pd.DataFrame
-        #self.assertIsInstance(actual_get_score, expected_get_score)
+    def test_save(self):
+        """
+               test method : we test if the output is an instance of dataframe
+        """
+        expected_get_score = pd.DataFrame
+        self.assertIsInstance(self.data.save(), expected_get_score)
